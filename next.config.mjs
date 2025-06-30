c07a229f7adea101d129eabbfbc92b6be10e5d9f
Rudra Sabnani <122001149+reaper718@users.noreply.github.com>
2025-06-30 21:29:56 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images:
-  {
-    domains: ["wp1.themevibrant.com"]
-  }
-};
+// next.config.mjs
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-export default nextConfig;
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["wp1.themevibrant.com"],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = __dirname
+    return config
+  },
+}
+
+export default nextConfig
